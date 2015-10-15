9ca3fe5bca643dad744280ebefad82480d02e0cd
jviding <jasu.viding@helsinki.fi>
2015-10-15 21:45:49 +0300

    portti säätö

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+http.listen(process.env.PORT || 3000, function(){ // pelkkä 3000
   console.log('Server listening on *:3000');
 });
 /*
