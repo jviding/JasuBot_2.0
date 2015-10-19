87af71133687b2790e91c74bf76381747fb29166
jviding <jasu.viding@helsinki.fi>
2015-10-19 08:02:58 +0300

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 module.exports = {
 
 	'facebookAuth' : {
-		'clientID' : '755208751268983',
-		'clientSecret' : 'bbf72ce140c75d524998ec6377acbeb5',
+		'clientID' : '',
+		'clientSecret' : '',
 		'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
 	}
 
