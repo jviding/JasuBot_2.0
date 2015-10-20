3381102ea53086991ac49945a178b17663f656dc
jviding <jasu.viding@helsinki.fi>
2015-10-20 11:20:47 +0300

    env va port pois

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = 3000;
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var passport = require('passport');
