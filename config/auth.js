69531240bedf26a5a35de4fa9366a572986d38d5
jviding <jasu.viding@helsinki.fi>
2015-10-24 08:11:18 +0300

    +

--- config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-	'facebookAuth' : {
-		'clientID' : '755208751268983',
-		'clientSecret' : 'bbf72ce140c75d524998ec6377acbeb5',
-		'callbackURL' : 'http://localhost:3000/auth/facebook/callback'
-	}
-
-};
